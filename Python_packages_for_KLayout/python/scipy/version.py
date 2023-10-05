da4afdd0a44cb4f60144230ef5ad126774a6351c
Fariha Ibnat Shaikh <fariha@lukas4.ece.ubc.ca>
2023-10-05 11:53:12 -0700

    Copied exact scipy package from container into here

--- Python_packages_for_KLayout/python/scipy/version.py
+++ Python_packages_for_KLayout/python/scipy/version.py
@@ -4,8 +4,8 @@
 short_version = '1.10.0'
 version = '1.10.0'
 full_version = '1.10.0'
-git_revision = 'dde5059'
-commit_count = '0'
+git_revision = 'Unknown'
+commit_count = 'Unknown'
 release = True
 
 if not release:
